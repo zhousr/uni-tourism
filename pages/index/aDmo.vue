a9222fcc152582ac50847b43f80ba79da3953205
周树荣 <zhoushurong@guiderank.org>
2020-03-19 09:35:03 +0800

    init

---
+++ pages/index/aDmo.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="content">
+    <!-- 高度自适应 -->
+    <image class="logo1"  mode="widthFix" src="/static/logo.png"></image> 
+    <!-- 比例不变，充满容器，超出剪裁 -->
+    <image class="logo2"  mode="widthFix" src="/static/logo.png"></image> 
+
+    <view class="text-area">
+      <text class="title">{{title}}</text>
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Hello'
+      }
+    },
+    onLoad() {
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style>
+.logo1{
+	width: 100%;
+	display: block;
+}
+.logo2{
+  width: 300rpx;
+  height: 200rpx;
+}
+</style>
